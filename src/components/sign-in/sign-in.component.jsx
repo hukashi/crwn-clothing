3a49cfba3528e997988a890a8045501e839d5c1c
Jonathan Chung <jonathan.chung@sooryen.com>
2020-07-09 15:53:29 -0400

    integrated firebase into application, updated styling and included sing out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,14 +52,15 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          <CustomButton type="submit" value="Submit Form">
-            Sign In
-          </CustomButton>
-
-          <CustomButton onClick={signInWithGoogle} value="Submit Form">
-            {" "}
-            Sign in with Google{" "}
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit" value="Submit Form">
+              Sign In
+            </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {" "}
+              Sign in with Google{" "}
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
